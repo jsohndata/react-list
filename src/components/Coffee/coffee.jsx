53c9404f55ebc354f643011f728b1903df79ab10
Jiho Sohn <jsohn@Jihos-MBP-2.attlocal.net>
2022-08-12 23:06:13 -0400

    first commit

---
+++ src/components/Coffee/coffee.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+const Coffee = () => {
+    const[getData, setData]= useState([])
+
+    const getCoffee = () => {
+        fetch('https://api.sampleapis.com/coffee/hot')
+        .then( response => response.json()  )
+        .then( responseData => setData(responseData) )
+        .catch( err => console.error(err) )
+    }
+
+    getCoffee()
+
+    return (
+        <>
+        <header>
+            <h1>Coffee</h1>
+        </header>
+        <main>
+            {getData.map( each => {
+                return(
+                    <div>
+                        <h3>{each.id} {each.title}</h3>
+                        <img src={each.image} alt='coffee' />
+                        <p>{each.description}</p>
+                    </div>
+                )
+            })}
+        </main>
+        </>
+    )
+}
+
+export default Coffee
